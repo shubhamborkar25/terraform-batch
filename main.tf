a870bfcda6791e3c1a5db14e1e7abe53937d6dd8
shubham <shubham@gmail.com>
2022-07-19 14:51:51 +0530

    ww

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_user" "this" {
+  name = "lenovo"
+
+  tags = {
+    tag-key = "tag-value"
+    name    = "lenovo_User-2"
+    env     = "development"
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name        = "terraform-admin-policy"
+  description = "this policy is belongs to terraform admin policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "*",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "this" {
+  user       = aws_iam_user.this.name
+  policy_arn = aws_iam_policy.this.arn
+  #   policy = aws_iam_policy.this.name
+
+  # policy_arn = aws_iam_policy.this.aws:iam::557909446472:policy/terraform-admin-policy
+
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.22.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-north-1"
+  profile = "terrform-batch"
+}
