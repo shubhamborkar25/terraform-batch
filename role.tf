676b052071271a443017b1a7e5035a58f898e76e
shubham <shubham@gmail.com>
2022-07-19 15:16:51 +0530

    --- shubham commit ---

--- role.tf
+++ role.tf
@@ -1,22 +1,22 @@
 resource "aws-iam-role" "this" {
-    name = "terraform-role"
-    assume_role_policy = jsonencode(
+  name = "terraform-role"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
         {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "sts:AssumeRole"
-            ],
-            "Principal": {
-                "Service": [
-                    "ec2.amazonaws.com"
-                ]
-            }
+          "Effect" : "Allow",
+          "Action" : [
+            "sts:AssumeRole"
+          ],
+          "Principal" : {
+            "Service" : [
+              "ec2.amazonaws.com"
+            ]
+          }
         }
-    ]
-}
-    )
-  
+      ]
+    }
+  )
+
 }
