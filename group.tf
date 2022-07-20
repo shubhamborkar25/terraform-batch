5c583ef4e70a3d9987664ad8f8f72f90dcd497b2
shubham <shubham_borkar@outlook.com>
2022-07-20 10:34:51 +0530

    --- shubham commit ---

--- group.tf
+++ group.tf
@@ -5,7 +5,7 @@
 
 # resource "aws_iam_user" "user1" {
 #     name = "developers"
-  
+
 # }
 # resource "aws_iam_group_membership" "this" {
 #     name = "group_members"
@@ -13,7 +13,7 @@
 #         aws_iam_user.user1.name,
 #         aws_iam_user.this.name
 #      ]
-    
+
 #     group = aws_iam_group.this.name
 
 # }
