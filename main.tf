26ece1ccb5e178415a1da88a020c6c6dde828ebe
shubham <shubham_borkar@outlook.com>
2022-07-20 10:32:41 +0530

    --- shubham commit ---

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-resource "aws_iam_policy" "this" {
-  name        = "terraform-admin-policy"
-  description = "this policy is belongs to terraform admin policy"
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : "*",
-        "Resource" : "*"
-      }
-    ]
-  })
-}
+# resource "aws_iam_policy" "this" {
+#   name        = "terraform-admin-policy"
+#   description = "this policy is belongs to terraform admin policy"
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Effect" : "Allow",
+#         "Action" : "*",
+#         "Resource" : "*"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_user_policy_attachment" "this" {
-  user       = aws_iam_user.this.name
-  policy_arn = aws_iam_policy.this.arn
-  #   policy = aws_iam_policy.this.name
+# resource "aws_iam_user_policy_attachment" "this" {
+#   user       = aws_iam_user.this.name
+#   policy_arn = aws_iam_policy.this.arn
+#   #   policy = aws_iam_policy.this.name
 
-  # policy_arn = aws_iam_policy.this.aws:iam::557909446472:policy/terraform-admin-policy
+#   # policy_arn = aws_iam_policy.this.aws:iam::557909446472:policy/terraform-admin-policy
 
-}
+# }
