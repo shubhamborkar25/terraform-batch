24d3e89f2af6232341fef6b6057835c346422194
shubham <shubham_borkar@outlook.com>
2022-07-20 11:27:44 +0530

    --- shubham commit ---

--- user.tf
+++ user.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_user" "this" {
   count = length(var.uname)
-  name = "${element(var.iam_users,count.index)}"
+  name  = element(var.iam_users, count.index)
   tags = {
     name = var.env
   }
