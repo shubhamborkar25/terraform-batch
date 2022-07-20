e27f851c4e90e33787fe6c2c1445d6aaedb55be2
shubham <shubham_borkar@outlook.com>
2022-07-20 11:36:46 +0530

    --- shubham commit ---

--- group.tf
+++ group.tf
@@ -1,14 +1,9 @@
-# create a group 1
+# create a group 
 resource "aws_iam_group" "group_1" {
   count = length(var.gname)
   name  = element(var.gname, count.index)
 }
 
-
-# resource "aws_iam_user" "user1" {
-#     name = "developers"
-
-# }
 # resource "aws_iam_group_membership" "this" {
 #     name = "group_members"
 #     users = [ 
