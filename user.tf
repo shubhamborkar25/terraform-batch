597e4ef91313b6ddc1b120d3409e5f0ac4d40972
shubham <shubham_borkar@outlook.com>
2022-07-20 10:31:21 +0530

    --- shubham commit ---

--- user.tf
+++ user.tf
@@ -1,9 +1,5 @@
 resource "aws_iam_user" "this" {
   name = "lenovo"
 
-  tags = {
-    tag-key = "tag-value"
-    name    = "lenovo_User-2"
-    env     = "development"
-  }
+  tags = var.string_var
 }
