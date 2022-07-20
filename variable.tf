e60626a6fa43a841d5e7ff0a230723e210bd682e
shubham <shubham_borkar@outlook.com>
2022-07-20 11:26:03 +0530

    --- shubham commit ---

--- variable.tf
+++ variable.tf
@@ -10,7 +10,7 @@ variable "uname" {
   type = list(any)
   default = ["user1", "user2",
   "user3", "user4", "user5", "user6",
-  "" ]
+  "user7", "user8", "user9", "user10"]
 }
 
 variable "num_var" {
