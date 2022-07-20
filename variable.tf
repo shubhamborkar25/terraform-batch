fd94291cd889fa2698df9b1e3b24d8bc960c8187
shubham <shubham_borkar@outlook.com>
2022-07-20 10:43:23 +0530

    --- shubham commit ---

--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
 # this is string variable
 
 variable "env" {
-  type    = string   
+  type    = string
   default = "dev"
 }
 
 # this is list variable
 variable "uname" {
-  type = list 
+  type    = list(any)
   default = ["user1", "user2", "user3", "user4"]
 }
