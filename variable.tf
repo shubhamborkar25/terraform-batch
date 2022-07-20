1c36897154a7cf88766c49e66dea3ca7ca98ed84
shubham <shubham_borkar@outlook.com>
2022-07-20 13:33:25 +0530

    --- shubham commit ---

--- variable.tf
+++ variable.tf
@@ -16,23 +16,22 @@ variable "uname" {
 variable "gname" {
   type    = list(any)
   default = ["group1", "group2"]
-
 }
-variable "num_var" {
-  type    = number
-  default = 1
+# variable "num_var" {
+#   type    = number
+#   default = 1
 
-}
+# }
 
-variable "map_var" {
-  type = map(any)
-  default = {
-    "a" : "user1",
-    "b" : "user2",
-    "c" : "user3"
+# variable "map_var" {
+#   type = map(any)
+#   default = {
+#     "a" : "user1",
+#     "b" : "user2",
+#     "c" : "user3"
 
-  }
-}
+#   }
+# }
 
 variable "group" {
   type    = list(any)
